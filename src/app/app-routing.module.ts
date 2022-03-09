95a734b988a452e31065080ea2dc47f948c7367a
alextasy <alex05tmm@gmail.com>
2022-03-09 23:07:55 +0200

    feat(*): creat offers home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OffersComponent } from './pages/offers/offers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: '', component: OffersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
