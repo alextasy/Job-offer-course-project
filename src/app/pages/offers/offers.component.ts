021bc30a1c7b48279ad31ed545bfafe2612e18e1
alextasy <alex05tmm@gmail.com>
2022-03-11 00:56:58 +0200

    feat(offers): implement offers page

--- src/app/pages/offers/offers.component.ts
+++ src/app/pages/offers/offers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IJobOffer } from 'src/app/interfaces/job-offer.interface';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-offers',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offers.component.scss']
 })
 export class OffersComponent implements OnInit {
+  public offers: IJobOffer[] = [];
 
-  constructor() { }
+  constructor(private data: DataService) { }
 
   ngOnInit(): void {
+    this.offers = this.data.offers;
   }
 
+  public getPostedBy(id: number): string {
+    return this.data.users.find(user => user.id === id)?.name || '';
+  }
+
+  public isOverflown({ clientWidth, clientHeight, scrollWidth, scrollHeight }: HTMLDivElement): boolean {
+    return scrollHeight > clientHeight || scrollWidth > clientWidth;
+  }
 }
