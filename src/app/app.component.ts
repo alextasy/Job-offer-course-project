4c0b51b19a82adebc8e85eeaf5a5e5e0fd2785ed
alextasy <alex05tmm@gmail.com>
2022-03-11 02:37:41 +0200

    feat(app): add sign in logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +8,28 @@ import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private modalService: NgbModal) {}
+  public errMessage = '';
+  constructor(
+    private modalService: NgbModal,
+    public data: DataService,
+  ) {}
 
   public openModal(content:any) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal'}).result.then((result) => {
-      // this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      // this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
+    this.modalService.open(content, {ariaLabelledBy: 'modal'});
+  }
+
+  public signIn(email: string, password: string): any {
+    if (!email) return this.errMessage = "Email is required!";
+    if (!password) return this.errMessage = "Password is required!";
+    const user = this.data.users.find(user => user.email === email && user.password === password);
+    if (!user) return this.errMessage = "Wrong credentials!"
+
+    this.data.currentUser = user;
+    this.errMessage = '';
+    this.modalService.dismissAll();
+  }
+
+  public signOut(): void {
+    this.data.currentUser = undefined;
   }
 }
